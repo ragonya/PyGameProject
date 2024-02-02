8ee63c4b486644ddd8f99c000cf82c19e7e53bfd
ragonya <141294973+ragonya@users.noreply.github.com>
2024-02-02 11:25:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import *
 from scripts import button
+import sys
 
 
 init()
@@ -17,24 +18,70 @@ PuzzleGame_txt = font.Font('Fonts/Honk-Regular-VariableFont_MORF,SHLN.ttf', 100)
 PuzzleGame_surface = PuzzleGame_txt.render('PuzzleGame', True, (230, 143, 85))
 main_font = font.Font('Fonts/Oswald-VariableFont_wght.ttf', 15)
 
+settings_txt = font.Font('Fonts/Honk-Regular-VariableFont_MORF,SHLN.ttf', 100)
+settings_surface = PuzzleGame_txt.render('Settings', True, (230, 143, 85))
+
 display.flip()
 play_button = button.Button(1200/2-(252/2), 200, 252, 74, "Play", 'images/buttons/static_button.png', 'images/buttons/hovered_button.png', 'sound_effects/button_clicked.mp3')
+difficulty_button = button.Button(1200/2-(252/2), 290, 252, 74, "Difficulty", 'images/buttons/static_button.png', 'images/buttons/hovered_button.png', 'sound_effects/button_clicked.mp3')
+settings_button = button.Button(1200/2-(252/2), 380, 252, 74, "Settings", 'images/buttons/static_button.png', 'images/buttons/hovered_button.png', 'sound_effects/button_clicked.mp3')
+exit_button = button.Button(1200/2-(252/2), 470, 252, 74, "Exit", 'images/buttons/static_button.png', 'images/buttons/hovered_button.png', 'sound_effects/button_clicked.mp3')
 
 
 def main_menu():
     running = True
     while running:
+        screen.fill((0, 0, 0))
         screen.blit(bg_main, (0, 0))
         screen.blit(PuzzleGame_surface, (350, 50))
-        play_button.draw(screen)
         for e in event.get():
             if e.type == QUIT:
                 running = False
-            play_button.handle_event(e)
+            if e.type == USEREVENT and e.button == settings_button:
+                settings_menu()
+                sys.exit()
 
-        play_button.check_hover(mouse.get_pos())
+            if e.type == USEREVENT and e.button == exit_button:
+                sys.exit()
+
+            for btn in [play_button, difficulty_button, settings_button, exit_button]:
+                btn.handle_event(e)
+
+        for btn in [play_button, difficulty_button, settings_button, exit_button]:
+            btn.draw(screen)
+            btn.check_hover(mouse.get_pos())
+        display.flip()
+
+def settings_menu():
+    audio_button = button.Button(1200/2-(252/2), 200, 252, 74, "Audio", 'images/buttons/static_button.png', 'images/buttons/hovered_button.png', 'sound_effects/button_clicked.mp3')
+    video_button = button.Button(1200/2-(252/2), 290, 252, 74, "Video", 'images/buttons/static_button.png', 'images/buttons/hovered_button.png', 'sound_effects/button_clicked.mp3')
+    back_button = button.Button(1200/2-(252/2), 380, 252, 74, "Back", 'images/buttons/static_button.png', 'images/buttons/hovered_button.png', 'sound_effects/button_clicked.mp3')
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        screen.blit(bg_settings, (0, 0))
+        screen.blit(settings_surface, (420, 50))
+        for e in event.get():
+            if e.type == QUIT:
+                running = False
+            if e.type == USEREVENT and e.button == back_button:
+                main_menu()
+                sys.exit()
+
+            for btn in [audio_button, video_button, back_button]:
+                btn.handle_event(e)
+
+        for btn in [audio_button, video_button, back_button]:
+            btn.draw(screen)
+            btn.check_hover(mouse.get_pos())
         display.flip()
 
 
-main_menu()
+
+
+def new_game():
+    pass
+
+if __name__ == '__main__':
+    main_menu()
 quit()
