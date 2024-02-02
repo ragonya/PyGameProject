9fdca03d481fdd7804e72974479d9a02bf820305
ragonya <141294973+ragonya@users.noreply.github.com>
2024-02-02 10:57:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,40 @@
 from pygame import *
-
-
-class Menu:
-    def __init__(self):
-        self.surfaces = []
-        self.callbacks = []
-        self.current_option_index = []
-
-    def append_option(self, option, callback):
-        self.current_option_index.append(calibri_50.render(option, True, Color('White')))
-        self.callbacks.append(callback)
-
-    def switch(self, direction):
-        self.current_option_index = max(0, min(self.current_option_index + direction, len(self.surfaces) - 1))
-
-    def select(self):
-        self.callbacks[self.current_option_index]()
-
-    def draw(self, surf, x, y, option_y_padding):
-        for i, option in enumerate(self.surfaces):
-            option_rect = option.get_rect()
-            option_rect.topleft = (x, y + i * option_y_padding)
-            if i == self.current_option_index:
-                draw.rect(surf, Color(0, 255, 0), option_rect)
-            surf.blit(option, option_rect)
+from scripts import button
 
 
 init()
-size = (800, 600)
+size = (1200, 800)
 screen = display.set_mode(size)
 display.set_caption('PuzzleGame')
-calibri_50 = font.SysFont('calibri', 50)
-screen.fill('BLACK')
-menu = Menu()
-menu.append_option('Hello world!', lambda: print('hello world!'))
-menu.append_option('Quit', quit)
-running = True
+
+icon = image.load('images/icon/icon.png')
+bg_main = image.load('images/backGround/bg_main.png')
+bg_settings = image.load('images/backGround/bg_settings.png')
+bg_help = image.load('images/backGround/bg_help.png')
+display.set_icon(icon)
+
+PuzzleGame_txt = font.Font('Fonts/Honk-Regular-VariableFont_MORF,SHLN.ttf', 100)
+PuzzleGame_surface = PuzzleGame_txt.render('PuzzleGame', True, (230, 143, 85))
+main_font = font.Font('Fonts/Oswald-VariableFont_wght.ttf', 15)
+
 display.flip()
-while running:
-    for e in event.get():
-        if e.type == QUIT:
-            running = False
-
-    menu.draw(screen, 100, 100, 75)
-    display.flip()
+play_button = button.Button(1200/2-(252/2), 200, 252, 74, "Play", 'images/buttons/static_button.png', 'images/buttons/hovered_button.png', 'sound_effects/button_clicked.mp3')
 
 
+def main_menu():
+    running = True
+    while running:
+        screen.blit(bg_main, (0, 0))
+        screen.blit(PuzzleGame_surface, (350, 50))
+        play_button.draw(screen)
+        for e in event.get():
+            if e.type == QUIT:
+                running = False
+            play_button.handle_event(e)
+
+        play_button.check_hover(mouse.get_pos())
+        display.flip()
+
+
+main_menu()
 quit()
-
-
