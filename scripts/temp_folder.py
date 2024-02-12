16c2dc06c7e9a8964952fe0d077ea09317f4bdaa
ragonya <141294973+ragonya@users.noreply.github.com>
2024-02-12 22:51:28 +0600

    Update temp_folder.py

--- scripts/temp_folder.py
+++ scripts/temp_folder.py
@@ -1,13 +1,12 @@
 import os
+import shutil
 from PIL import Image
 
 
 class folders:
     def create_folder():  # создание временной папки
-        if not os.path.isdir('temp_folder'):
-            os.mkdir('temp_folder')
-        else:
-            pass
+        shutil.rmtree(os.path.abspath('temp_folder'))
+        os.mkdir('temp_folder')
 
     def split_image_into_squares(image_path, save_dir, num_squares):  # алгоритм разбития
         global square_size
@@ -20,19 +19,19 @@ class folders:
 
         for y in range(1, num_squares + 1):
             for x in range(num_squares):
-                left = x * square_size
-                upper = y * square_size
+                left = x * square_size + 200
+                upper = y * square_size - 200
                 right = left + square_size
                 lower = upper + square_size
                 square = image.crop((left, upper, right, lower))
                 square.save(os.path.join(save_dir, f"square_{num_squares * (y - 1) + x + 1}.jpg"))
 
-    def make_sqares(sq1):  # создание мозаики
-        image_path = "images/images_to_pazzle/file.png"
+    def make_sqares(sq1, current_image):  # создание мозаики
+        image_path = f"images/images_to_pazzle/{current_image}.jpg"
         save_dir = "temp_folder"
         num_squares = sq1
         folders.split_image_into_squares(image_path, save_dir, num_squares)
 
-    def main(sq):
+    def main(sq, image):
         folders.create_folder()
-        folders.make_sqares(sq)
+        folders.make_sqares(sq, image)
